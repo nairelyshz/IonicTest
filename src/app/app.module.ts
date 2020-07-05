daf7ada1492e9117f1d41bc2fb5dc197854dae4e
nairelyhz <njhs694@gmail.com>
2020-07-05 16:13:40 -0400

    features app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,16 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AngularFireModule } from '@angular/fire';
+import {  environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, 
+    IonicModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebaseConfig), 
+    AppRoutingModule],
   providers: [
     StatusBar,
     SplashScreen,
