9b8238d71f646d8de86540f86c0aa3411a2e6e1f
nairelyhz <njhs694@gmail.com>
2020-07-05 16:11:54 -0400

    add auth services

---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/auth';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  public isLogged: any = false;
+
+  constructor(private fAuth: AngularFireAuth) {
+    this.fAuth.authState.subscribe((user)=>(this.isLogged = user));
+  }
+
+  public async loginFirebase(userData){
+    console.log("login");
+    try{
+      return await this.fAuth.signInWithEmailAndPassword(userData.email, userData.password);
+    }catch (error) {
+      console.log('Error log in ', error);
+    }
+  }
+
+  public async registerFirebase(userData){
+    console.log(userData);
+    try{
+      return await this.fAuth.createUserWithEmailAndPassword(userData.email, userData.password);
+    }catch (error) {
+      console.log('Error log in ', error);
+    }
+  }
+}
