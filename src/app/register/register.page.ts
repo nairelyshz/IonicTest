b135239ef64d5614c447333ea24fceaeb419fd31
nairelyhz <njhs694@gmail.com>
2020-07-05 16:14:09 -0400

    add register page

--- src/app/register/register.page.ts
+++ src/app/register/register.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.page.scss'],
 })
 export class RegisterPage implements OnInit {
-
-  constructor() { }
+  registerForm: FormGroup;
+  constructor(private formBuilder: FormBuilder, private auth: AuthService) { }
 
   ngOnInit() {
+    console.log("*****************");
+    this.registerForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    }); 
+  }
+
+  register(){
+    console.log("QUE PASA");
+    console.log(this.registerForm.value);
+    this.auth.registerFirebase(this.registerForm.value).then(register => {
+      console.log(register);
+    });
   }
 
 }
