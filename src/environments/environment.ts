b74466ff5035ab06e965624002339a16edbea26a
nairelyhz <njhs694@gmail.com>
2020-07-05 16:15:15 -0400

    add apis

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,19 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyDk3Kd0SR7Y0iU-FE4yel4__2Wjc-ZIrXA",
+    authDomain: "humantech-16401.firebaseapp.com",
+    databaseURL: "https://humantech-16401.firebaseio.com",
+    projectId: "humantech-16401",
+    storageBucket: "humantech-16401.appspot.com",
+    messagingSenderId: "907751889367",
+    appId: "1:907751889367:web:803bc386152eb09d70d392",
+    measurementId: "G-N1G3V387BX"
+  },
+  apiService: "https://rickandmortyapi.com/api/"
+
 };
 
 /*
